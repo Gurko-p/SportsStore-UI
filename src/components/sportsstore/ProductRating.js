886a6200b6f2aa6ac886d3c93ccfdc1339637e94
gurko-p <gurko-p@mail.ru>
2024-10-22 14:35:20 +0300

    Settings of SignalR has been changed in ProductRating component

--- src/components/sportsstore/ProductRating.js
+++ src/components/sportsstore/ProductRating.js
@@ -1,64 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import { Rating, Box, Typography } from '@mui/material';
 import { HubConnectionBuilder } from '@microsoft/signalr';
+import { baseURL } from '../../api/urls';
 
 const ProductRating = ({ product }) => {
-    // const [averageRating, setAverageRating] = useState(0);
-    // const [connection, setConnection] = useState(null);
+  const [averageRating, setAverageRating] = useState(0);
+  const [connection, setConnection] = useState(null);
 
-    // useEffect(() => {
-    //     try{
-    //         const connect = new HubConnectionBuilder()
-    //         .withUrl("https://localhost:7046/ratingHub")
-    //         .build();
-    //     console.log(connect)
-    //     setConnection(connect);
+  useEffect(() => {
+    const connect = new HubConnectionBuilder()
+      .withUrl(`${baseURL}ratingHub`)
+      .build();
+    connect?.on('ReceiveRating', (product) => {
+      console.log("соединение с хабом установлено")
+      //setAverageRating(product.averageRating);
+    });
+    setConnection(connect);
+  }, []);
 
-    //     connect?.start()
-    //     .then(() => console.log('Connected to SignalR hub'))
-    //     .catch(err => console.log('Error while starting connection: ' + err));
+  useEffect(() => {
+    if (connection) {
+      connection?.start()
+        .then(() => console.log('Connected to SignalR hub'))
+        .catch(err => console.log('Error while starting connection: ' + err));
 
-    //     connect?.on('ReceiveRating', (product) => {
-    //     setAverageRating(product.averageRating);
-    //     });
+      return () => {
+        connection?.stop();
+      };
+    }
+  }, [connection]);
 
-    //     return () => {
-    //         connect?.stop();
-    //     };
-    //     }
-    //     catch(er){
-    //         console.log(er);
-    //     }
-        
-    // }, []);
+  const handleRatingChange = async (newValue) => {
+    console.log(newValue, "newValue");
+    // await fetch('/api/products/rate', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({ productId: product.id, rating: newValue }),
+    // });
+  };
 
-    const handleRatingChange = async (newValue) => {
-        console.log(newValue, "newValue");
-        // await fetch('/api/products/rate', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify({ productId: product.id, rating: newValue }),
-        // });
-    };
-
-    return (
-        <div>
-              <Box sx={{ display: "flex", justifyContent: "center" }}>
-                <Rating
-                  name="product-rating"
-                  precision={0.5}
-                  value={product?.rating?.overallRating ?? 0}
-                  onChange={(event, newValue) => handleRatingChange(newValue)}
-                />
-                <Typography sx={{ color: "text.secondary" }}>
-                    ({product?.rating?.overallRating ?? 0}) (
-                    {product?.rating?.totalRates ?? "нет оценок"})
-                </Typography>
-              </Box>
-        </div>
-    );
+  return (
+    <div>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Rating
+          name="product-rating"
+          precision={0.5}
+          value={product?.rating?.overallRating ?? 0}
+          onChange={(event, newValue) => handleRatingChange(newValue)}
+        />
+        <Typography sx={{ color: "text.secondary" }}>
+          ({product?.rating?.overallRating ?? 0}) (
+          {product?.rating?.totalRates ?? "нет оценок"})
+        </Typography>
+      </Box>
+    </div>
+  );
 };
 
 export default ProductRating;
