1b87a73328f1cec2a28f07a68eba47034bab84c9
gurko-p <gurko-p@mail.ru>
2024-10-24 15:56:04 +0300

    Added ProgressLoader component

---
+++ src/components/loader/ProgressLoader.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+export default function ProgressLoader({ loading }) {
+  return (
+    <Box
+      sx={{
+        display: loading ? 'flex' : 'none',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(255, 255, 255, 0.7)',
+        zIndex: 9999,
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
+}
