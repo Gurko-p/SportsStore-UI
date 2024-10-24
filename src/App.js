a6c4686af1c13cef7523f5c1c38109caa26af642
gurko-p <gurko-p@mail.ru>
2024-10-24 16:49:45 +0300

    The ProgressLoader component is set as global. It is launched when an http request is sent, when the response is received the loader is hidden

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import {
 import SnackBarComponent from "./components/snackBar/SnackBar";
 import { useSelector } from "react-redux";
 import router from './routes';
+import ProgressLoader from "./components/loader/ProgressLoader";
 
 
 function App() {
   const { open, message, severity } = useSelector((state) => state.alert);
+  const { isLoading } = useSelector((state) => state.loader);
   return (
     <>
       <RouterProvider router={router} />
+      <ProgressLoader loading={isLoading} />
       <SnackBarComponent open={open} message={message} severity={severity} />
     </>
   );
--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { baseURL } from './urls';
 import { removeLoggedIn } from '../features/auth/authSlice';
+import { showLoader, hideLoader } from '../features/loader/loaderSlice';
 import store from "../app/store";
 import { alertService, severity } from '../components/snackBar/alertService';
 
@@ -12,6 +13,7 @@ const axiosInstance = axios.create({
 //Добавляем интерсептор запроса
 axiosInstance.interceptors.request.use(
   (config) => {
+      store.dispatch(showLoader());
       const token = localStorage.getItem('token');
       if (token) {
           config.headers.Authorization = `Bearer ${token}`; // Добавляем заголовок Authorization
@@ -19,6 +21,7 @@ axiosInstance.interceptors.request.use(
       return config;
   },
   (error) => {
+    store.dispatch(hideLoader());
     console.log(error, "error");
     alertService.show(`Ошибка сети или сервер недоступен. Обратитесь к системному администратору.`, severity.error, 10000);
   }
@@ -27,15 +30,16 @@ axiosInstance.interceptors.request.use(
 //Добавляем интерсептор ответа
 axiosInstance.interceptors.response.use(
   response => {
+    store.dispatch(hideLoader());
     return response;
   },
   error => {
     const { response } = error;
+    store.dispatch(hideLoader());
     if (response) {
       if (response.status === 401) {
         console.error('Ошибка 401: Необходима авторизация. Вероятно истек срок действия токена авторизации.');
         store.dispatch(removeLoggedIn());
-        
         return [];
       }
       if (response.status === 403) {
